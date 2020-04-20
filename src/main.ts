6689ec3f89f05c06949913cab066b1dbfeb4f9cf
Sophia Guo <sophiag@ca.ibm.com>
2020-04-20 11:48:13 -0400

    Test Case4 : test github-hosted JDK multiplatform
    Signed-off-by: Sophia Guo <sophiag@ca.ibm.com>

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,21 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as tc from '@actions/tool-cache'
+import * as exec from '@actions/exec'
+import * as io from '@actions/io'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`)
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    const version = '14'
+    const bootjdkVersion = (parseInt(version) - 1).toString()
+    const bootjdkJar = await tc.downloadTool(`https://api.adoptopenjdk.net/v2/binary/releases/openjdk${bootjdkVersion}?openjdk_impl=openj9&os=linux&arch=x64&release=latest&heap_size=normal&type=jdk`)
+    await io.mkdirP('bootjdk')
+    await exec.exec('ls')
+    await exec.exec(`sudo tar -xzf ${bootjdkJar} -C ./bootjdk --strip=1`)
+ //   await tc.extractTar(`${bootjdk13}`, `${workDir}/bootjdk13`, '-xzf --strip-components=1')
+    await io.rmRF(`${bootjdkJar}`)
+    await exec.exec('ls')
+    process.chdir('bootjdk13')
+    await exec.exec('ls')
   } catch (error) {
     core.setFailed(error.message)
   }
