6f7fcbfba590090f76c85513bc40e241ea692a53
Sophia Guo <sophiag@ca.ibm.com>
2020-04-23 16:42:08 -0400

    Test non openjdk-tests nor TKG repo
    Signed-off-by: Sophia Guo <sophiag@ca.ibm.com>

--- src/main.ts
+++ src/main.ts
@@ -5,16 +5,7 @@ import * as io from '@actions/io'
 
 async function run(): Promise<void> {
   try {
-    const version = '14'
-    const bootjdkVersion = (parseInt(version) - 1).toString()
-    const bootjdkJar = await tc.downloadTool(`https://api.adoptopenjdk.net/v2/binary/releases/openjdk${bootjdkVersion}?openjdk_impl=openj9&os=linux&arch=x64&release=latest&heap_size=normal&type=jdk`)
-    await io.mkdirP('bootjdk')
-    await exec.exec('ls')
-    await exec.exec(`sudo tar -xzf ${bootjdkJar} -C ./bootjdk --strip=1`)
- //   await tc.extractTar(`${bootjdk13}`, `${workDir}/bootjdk13`, '-xzf --strip-components=1')
-    await io.rmRF(`${bootjdkJar}`)
-    await exec.exec('ls')
-    process.chdir('bootjdk13')
+    core.info(`git_ref is ${process.env.GITHUB_REF}`)
     await exec.exec('ls')
   } catch (error) {
     core.setFailed(error.message)
