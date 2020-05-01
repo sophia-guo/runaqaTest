c432cb85712c5a12cf44a836e42b015b0526bcd8
Sophia Guo <sophiag@ca.ibm.com>
2020-05-01 16:21:37 -0400

    test

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,12 @@ import * as io from '@actions/io'
 
 async function run(): Promise<void> {
   try {
-    const defaultMyInput = core.getInput('myInput')
-  //  const defaultMyInput = core.getInput('myInput') === 'true'
-    core.info(`what is defatul ${defaultMyInput}`) 
+  //  const defaultMyInput = core.getInput('myInput')
+    await io.mkdirP('jdk')
+    process.chdir('jdk')
+    await io.mkdirP('boot')
+    await io.mkdirP('home')
+    core.setOutput('BuildJDKDir', `${process.env['GITHUB_WORKSPACE']}/jdk/boot`)
   } catch (error) {
     core.setFailed(error.message)
   }
