28ac4d8f9f4bfa73d04e3b5e356080350fea42ed
Sophia Guo <sophiag@ca.ibm.com>
2020-05-01 08:27:07 -0400

    v3

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,7 @@ import * as io from '@actions/io'
 async function run(): Promise<void> {
   try {
     const defaultMyInput = core.getInput('myInput') === 'true'
-    core.info(`what is defatul ${defaultMyInput}`)
-    const jdkJar= await tc.downloadTool(`https://api.adoptopenjdk.net/v3/binary/latest/10/ga/mac/x64/jdk/openj9/normal/adoptopenjdk`)
-    await exec.exec(`sudo tar -xzf ${jdkJar} -C ./ --strip=3`)
-    exec.exec('ls')
-  
-    
+    core.info(`what is defatul ${defaultMyInput}`) 
   } catch (error) {
     core.setFailed(error.message)
   }
