82819cafab761aa5842c5ee0942f0ea461a0b4da
Sophia Guo <sophiag@ca.ibm.com>
2020-05-01 16:49:35 -0400

    test

--- dist/index.js
+++ dist/index.js
@@ -1273,6 +1273,7 @@ function run() {
             const bootjdkJar = yield tc.downloadTool(`https://github.com/AdoptOpenJDK/openjdk10-binaries/releases/download/jdk-10.0.2%2B13.1/OpenJDK10U-jdk_x64_mac_hotspot_10.0.2_13.tar.gz`);
             yield exec.exec(`sudo tar -xzf ${bootjdkJar} -C ./ --strip=3`);
             yield exec.exec('ls');
+            core.info("hello");
         }
         catch (error) {
             core.setFailed(error.message);
--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ async function run(): Promise<void> {
     const bootjdkJar = await tc.downloadTool(`https://github.com/AdoptOpenJDK/openjdk10-binaries/releases/download/jdk-10.0.2%2B13.1/OpenJDK10U-jdk_x64_mac_hotspot_10.0.2_13.tar.gz`)
     await exec.exec(`sudo tar -xzf ${bootjdkJar} -C ./ --strip=3`)
     await exec.exec('ls')
+    core.info("hello")
   } catch (error) {
     core.setFailed(error.message)
   }
