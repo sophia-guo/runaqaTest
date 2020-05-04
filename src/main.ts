acfb10d39a36eb94b13c4a2d8744cc64f52d4da4
Sophia Guo <sophiag@ca.ibm.com>
2020-05-04 13:43:06 -0400

    test path

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,10 @@ async function run(): Promise<void> {
     core.info(`path is ${process.env['PATH']}`)
     exec.exec('printenv')
     exec.exec('tar --version')
+    const freeMarker = await tc.downloadTool(`https://sourceforge.net/projects/freemarker/files/freemarker/2.3.8/freemarker-2.3.8.tar.gz/download`)
+    await exec.exec(`sudo tar -xzf ${freeMarker} freemarker-2.3.8/lib/freemarker.jar --strip=2`)
+    await io.rmRF(`${freeMarker}`)
+    exec.exec('ls')
 
   } catch (error) {
     core.setFailed(error.message)
